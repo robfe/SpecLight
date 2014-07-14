7c632cca508d2b6c7a741c6e7a46067a33a8e9e9
Robert Fonseca-Ensor <rfe@element26software.com>
2014-07-14 14:19:42 +1200

    nice formatting for should_, false etc

--- SpecLight.Tests/StringHelpersTests.cs
+++ SpecLight.Tests/StringHelpersTests.cs
@@ -9,5 +9,12 @@ namespace SpecLight.Tests
 		{
 			Assert.Equal("the US is one word", StringHelpers.CreateText("The_IsOneWord", "US"));
 		}
+
+		[Fact]
+		public void ShouldTrueGetsReplaced()
+		{
+			Assert.Equal("shan't", StringHelpers.CreateText("Shall_", false));
+			Assert.Equal("I shouldn't see the light", StringHelpers.CreateText("IShould_SeeTheLight", false));
+		}
 	}
 }
--- SpecLight/StringHelpers.cs
+++ SpecLight/StringHelpers.cs
@@ -1,12 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SpecLight
 {
     static class StringHelpers
     {
+	    static readonly Dictionary<string, string> Replacements = @"
+i=I
+i've=I've
+should False=shouldn't
+should True=should
+will False=won't
+will True=will
+can False=can't
+can True=can
+shall False=shan't
+shall True=shall
+is False=isn't
+is True=is
+".Trim().Split('\n').Select(x => x.Trim().Split('=')).ToDictionary(x => x[0], x => x[1]);
+
         internal static string CreateText(string name, params object[] args)
         {
             var parameterQueue = new Queue<string>(args.Select(x => x.ToString()));
@@ -19,10 +35,16 @@ namespace SpecLight
                 paramsSubsituted += "(" + string.Join(", ", parameterQueue) + ")";
             }
 
-
-			var fixI = (paramsSubsituted + " ").Replace(" i ", " I ").Replace(" ive ", " I've ");
-            var normalizeSpaces = Regex.Replace(fixI, "\\s+", " ");
+			var sb = new StringBuilder(paramsSubsituted);
+	        foreach (var kvp in Replacements)
+	        {
+		        sb.Replace(" " + kvp.Key + " ", " " + kvp.Value + " ");
+	        }
+            var normalizeSpaces = Regex.Replace(sb.ToString(), "\\s+", " ");
             return normalizeSpaces.Trim();
         }
+
+
     }
+
 }
