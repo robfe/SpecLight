ee14be56947cefaf496197ff562964d3f4e402d8
Cara Fonseca-Ensor <cara.fonseca-ensor@pushpay.com>
2017-06-27 20:49:00 +1200

    Add System.Web.WebPages.HelperResult implementation for .Net Core

--- SpecLight/Output/RazorTemplateBase.cs
+++ SpecLight/Output/RazorTemplateBase.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Web.WebPages;
 using SpecLight.Output.ViewModel;
 
+
 namespace SpecLight.Output
 {
     internal class RazorTemplateBase
@@ -108,3 +109,43 @@ namespace SpecLight.Output
         }
     }
 }
+
+
+#if NETCOREAPP1_1
+namespace System.Web.WebPages
+{
+    public class HelperResult
+    {
+        private readonly Action<TextWriter> _action;
+
+        public HelperResult(Action<TextWriter> action)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+            _action = action;
+        }
+
+        public string ToHtmlString()
+        {
+            return ToString();
+        }
+
+        public override string ToString()
+        {
+            using (var writer = new StringWriter(CultureInfo.InvariantCulture))
+            {
+                _action(writer);
+                return writer.ToString();
+            }
+        }
+
+        public void WriteTo(TextWriter writer)
+        {
+            _action(writer);
+        }
+    }
+
+}
+#endif
