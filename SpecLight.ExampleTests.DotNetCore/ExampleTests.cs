f84a38c9c795fa0872f3a7a8f886f6a6fd1f32c3
Cara Fonseca-Ensor <cara.fonseca-ensor@pushpay.com>
2017-06-28 13:08:01 +1200

    Added example tests project for .Net Core

---
+++ SpecLight.ExampleTests.DotNetCore/ExampleTests.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace SpecLight.ExampleTests.DotNetCore
+{
+    [Trait("category", "examples")]
+    [Description(@"
+# Simple examples
+
+These tests demonstrate how [SpecLight](https://github.com/robfe/speclight) works. Some of them fail on purpose, so that you can see how SpecLight behaves for failing tests.
+
+They demonstrate:
+
+* Failing steps
+* Pending steps
+* Passing steps
+* Empty steps (If a step has no implementation, it will pass but SpecLight will tell you it was empty)
+* Parameterised tests (`TheoryAttribute` in `xUnit`, but the same principle applies to other frameworks). These *replace* BDD-style `Scenario Outlines`
+* Fixtures (Aspects that can be attached to Specs by calling WithFixture)
+
+")]
+    public class ExampleTests
+    {
+        List<int> numbers = new List<int>();
+        int total;
+            
+        [Fact]
+        public void Pending()
+        {
+            new Spec(@"
+                    Here is a spec where one of the steps will throw a NotImplementedException, causing the result of 'pending'.
+
+                    In order to know how much money I can save
+                    As a Math Idiot
+                    I want to add two numbers").Tag("Pending")
+                .Given(IEnter_, 5)
+                .And(IEnter_, 6)
+                .When(ICallAMethodThatsNotImplemented).Tag("NotImplemented")
+                .Then(TheResultShouldBe_, 11)
+                .Execute();
+        }
+
+        [Fact]
+        public void Passing()
+        {
+            new Spec(@"
+                    Here is a spec that should pass
+
+                    In order to know how much money I can save
+                    As a Math Idiot
+                    I want to add two numbers").Tag("Money")
+                .Given(IEnter_, 5)
+                .And(IEnter_, 6)
+                .When(IPressAdd)
+                .Then(TheResultShouldBe_, 11)
+                .Execute();
+        }
+
+        [Fact]
+        public void Empty()
+        {
+            new Spec(@"
+                    Sometimes you just want to write a step, and have it pass even though it does nothing
+                    SpecLight detects methods that have no code and adds 'empty' to the status of 'passed'")
+                .Given(EmptyMethodWithArgument_, "x")
+                .And(EmptyMethodWithNoArgument)
+                .Execute();
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, -2, -1)]
+        [InlineData(1, 2, 35)]
+        public void Theory(int i1, int i2, int sum)
+        {
+            new Spec(@"
+                    SpecLight supports parameterized tests, but it helps if you pass a 'name' to the Execute method
+
+                    In order to know how much money I can save
+                    As a Math Idiot
+                    I want to add two numbers").Tag("Money")
+                .Given(IEnter_, i1)
+                .And(IEnter_, i2)
+                .When(IPressAdd)
+                .Then(TheResultShouldBe_, sum)
+                .Execute(string.Format("Theory: {0}+{1}={2}", i1, i2, sum));
+        }
+
+        [Fact]
+        public void Failing()
+        {
+            new Spec(@"
+                    Here is a spec that should fail. Steps after the failing step are skipped
+                    
+
+                    In order to know how much money I can save
+                    As a Math Idiot
+                    I want to add two numbers")
+                .Tag("DemonstrateFinally")
+                .WithFixture<ExecutionTimer>()
+                .Given(IEnter_, 5)
+                .Finally(() => Console.WriteLine("Cleanup 1/2"))
+                .And(IEnter_, 6)
+                .When(IPressAdd)
+                .Then(TheResultShouldBe_, -12013)
+                .And(ICallAMethodThatsNotImplemented)
+                .Finally(() => Console.WriteLine("Cleanup 2/2"))
+                .Execute();
+        }
+
+
+        void IPressAdd()
+        {
+            total = numbers.Sum();
+        }
+
+        void TheResultShouldBe_(int obj)
+        {
+            Assert.Equal(obj, total);
+        }
+
+        void ICallAMethodThatsNotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+        void EmptyMethodWithArgument_(string arg)
+        {
+
+        }
+
+        void EmptyMethodWithNoArgument()
+        {
+
+        }
+
+        void IEnter_(int obj)
+        {
+            numbers.Add(obj);
+        }
+    }
+
+    public class ExecutionTimer : SpecFixtureBase
+    {
+        public override void StepSetup(Step step)
+        {
+            //since this class is time-sensitive, access the dictionary not the bag. Warmup can be as high as 200ms for dynamic invokes.
+            step.DataDictionary["Timer"] = Stopwatch.StartNew(); //this won't get printed as it's not a string.
+        }
+
+        public override void StepTeardown(Step step)
+        {
+            var stopwatch = (Stopwatch)step.DataDictionary["Timer"];
+            stopwatch.Stop();
+            step.DataBag.ExecutionTime = stopwatch.ElapsedMilliseconds + "ms"; //this goes into the output as it's a string
+
+            if (stopwatch.ElapsedMilliseconds > 10)
+            {
+                step.Tags.Add("slow");
+            }
+        }
+
+        public override void SpecTeardown(Spec spec)
+        {
+            //note that the spec would have actually taken a lot longer to execute (SpecLight reflection overhead), but the following number is accurate for user code
+            var total = spec.Steps.Select(x => (Stopwatch)x.DataBag.Timer).Sum(x => x.ElapsedMilliseconds);
+            spec.DataBag.ExecutionTime = total + "ms";
+        }
+    }
+}
---
+++ SpecLight.ExampleTests.DotNetCore/TaskBasedAsyncTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpecLight.ExampleTests.DotNetCore
+{
+    [Trait("category", "examples")]
+    [Description(@"
+# Async support
+
+[SpecLight](https://github.com/robfe/speclight) can be used to create async tests. 
+
+Calling `(Given/When/Then/And)Async` lets you return a Task from your step method, which SpecLight will wait for before calling the next step
+
+Then you should `ExecuteAsync` the Spec instead of simply calling `Execute`, so that you can pass the parent task up to your test framework.
+")]
+
+    public class TaskBasedAsyncTests
+    {
+        [Fact]
+        public Task Async()
+        {
+            //return a task
+            return new Spec(@"
+SpecLight can be used to execute async methods, chaining the Task awaiting all the way up to the test framework")
+                .WithFixture<ExecutionTimer>()
+                .Tag("async")
+                .GivenAsync(ICallAnAsyncMethod)
+                .WhenAsync(ICallAnotherAsyncMethodThatWaitsFor_Msec, 15)
+                .Then(ICanStillCallASynchronousMethod)
+                .ExecuteAsync();
+        }        
+        
+        [Fact]
+        public async Task AwaitableAsync()
+        {
+            //await a task
+            await new Spec(@"
+It's up to you whether you `await` the call to `ExecuteAsync()` or return it directly")
+                .WithFixture<ExecutionTimer>()
+                .Tag("async")
+                .GivenAsync(ICallAnAsyncMethod)
+                .WhenAsync(ICallAnotherAsyncMethodThatWaitsFor_Msec, 15)
+                .Then(ICanStillCallASynchronousMethod)
+                .ExecuteAsync();
+        }
+
+        [Fact]
+        public void AsyncWarnsIfYouDontCallAsync()
+        {
+            new Spec(@"Testing frameworks like to be given a Task if you're using them, so SpecLight will warn you (with Obsolete) if you call Execute on a spec that's used an async step")
+                .WithFixture<ExecutionTimer>()
+                .Tag("async")
+                .GivenAsync(ICallAnAsyncMethod)
+                .WhenAsync(ICallAnotherAsyncMethodThatWaitsFor_Msec, 15)
+                .Then(ICanStillCallASynchronousMethod)
+                .Execute(); //heed this warning
+        }
+
+        [Fact]
+        public Task AsyncFailure()
+        {
+            //return a task
+            return new Spec(@"
+SpecLight can be used to execute async methods, chaining the Task awaiting all the way up to the test framework")
+                .WithFixture<ExecutionTimer>()
+                .Tag("async")
+                .GivenAsync(ICallAnAsyncMethod)
+                .WhenAsync(ICallAnotherAsyncMethodThatWaitsFor_Msec, 15)
+                .ThenAsync(ICallAFailingAsyncMethod)
+                .ExecuteAsync();
+        }
+
+        Task ICallAFailingAsyncMethod()
+        {
+//            throw new Exception();
+            return Task.Factory.StartNew(() => { throw new Exception(); });
+        }
+
+
+        async Task ICallAnAsyncMethod()
+        {
+            Console.Out.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(5);
+            Console.Out.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        Task ICallAnotherAsyncMethodThatWaitsFor_Msec(int msec)
+        {
+            Console.Out.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            return Task.Delay(msec);
+        }
+
+        void ICanStillCallASynchronousMethod()
+        {
+            Console.Out.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
